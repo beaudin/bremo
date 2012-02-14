9cdb54d8bdfa70aff6fde5bb922c426b27c5ea2a
ngueneko <ngueneko@gmail.com>
2012-02-14 19:59:38 +0000

    Bremo Thread Eingeführt

--- Bremo/src/bremoswing/SwingBremo.java
+++ Bremo/src/bremoswing/SwingBremo.java
@@ -1,31 +1,42 @@
 package bremoswing;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.File;
 import java.io.PrintStream;
 
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.ScrollPaneConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.TitledBorder;
 
 import bremo.main.Bremo;
-import bremoswing.*;
 
 /*
  * SwingBremo.java
@@ -36,8 +47,61 @@ import bremoswing.*;
  * 
  * @author Ngueneko Steve
  */ 
-public class SwingBremo extends javax.swing.JFrame {
+public class SwingBremo extends JFrame {
 
+	
+	// Variables declaration
+		private boolean control = false;
+		private File file;
+		private File [] files;
+		private String path = ".";
+		private Bremo bremo;
+		private Bremo [] bremoThread;
+		public static JButton berechnen;
+		private JTextArea grosArea;
+		private JScrollPane jScrollPane1;
+		private JScrollPane jScrollPane2;
+		private JTextArea kleinArea;
+		private JPanel konsole2;
+		private JLayeredPane manager;
+		public static  JButton stop;
+		private JTextField textFile;
+		public static  JButton wahlFile;
+		private double t1;
+		private double t2;
+		PrintStream outStream = new PrintStream(System.out) {
+
+			@Override
+			public void  println(String s) {
+
+				grosArea.append(s + "\n");
+				grosArea.setCaretPosition(grosArea.getDocument().getLength());
+			}
+
+			@Override
+			public void print(String s) {
+
+				grosArea.append(s);
+				grosArea.setCaretPosition(grosArea.getDocument().getLength());
+
+			}
+		};
+		PrintStream errStream = new PrintStream(System.err) {
+
+			@Override
+			public void println(String s) {
+				kleinArea.append(s + "\n");
+				kleinArea.setCaretPosition(kleinArea.getDocument().getLength());
+			}
+
+			@Override
+			public void print(String s) {
+				kleinArea.append(s);
+				kleinArea.setCaretPosition(kleinArea.getDocument().getLength());
+			}
+		};
+	// End of variables declaration
+		
 	/** Creates new form SwingBremo */
 	public SwingBremo() {
 		initComponents();
@@ -53,53 +117,53 @@ public class SwingBremo extends javax.swing.JFrame {
 		System.setOut(outStream);
 		System.setErr(errStream);
 
-		manager = new javax.swing.JLayeredPane();
-		berechnen = new javax.swing.JButton();
-		wahlFile = new javax.swing.JButton();
-		stop = new javax.swing.JButton();
-		textFile = new javax.swing.JTextField();
+		manager = new JLayeredPane();
+		berechnen = new JButton();
+		wahlFile = new JButton();
+		stop = new JButton();
+		textFile = new JTextField();
         konsole2 = new JPanel();
-		grosArea = new javax.swing.JTextArea();
-		jScrollPane1 = new javax.swing.JScrollPane();
-		kleinArea = new javax.swing.JTextArea();
-		jScrollPane2 = new javax.swing.JScrollPane();
+		grosArea = new JTextArea();
+		jScrollPane1 = new JScrollPane();
+		kleinArea = new JTextArea();
+		jScrollPane2 = new JScrollPane();
 		
 		
 
-		setTitle("Bremo");
-		setBackground(new java.awt.Color(255, 255, 255));
-		setIconImage(new javax.swing.ImageIcon(getClass().getResource(
+		setTitle("Bremo 1.0");
+		setBackground(new Color(255, 255, 255));
+		setIconImage(new ImageIcon(getClass().getResource(
 				"/bremoswing/bild/bremo1.png")).getImage());
 		// setLocationByPlatform(true);
 		setName("BremoGraphik"); // NOI18N
 		setResizable(true);
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		
-		 this.addWindowListener(new java.awt.event.WindowAdapter() { 
-			 public void windowClosing(java.awt.event.WindowEvent e) { 
+		 this.addWindowListener(new WindowAdapter() { 
+			 public void windowClosing(WindowEvent e) { 
 				 closeFrame();
 				 }
 			 });
 			 
 		 
-		manager.setBorder(javax.swing.BorderFactory.createTitledBorder(null,
+		manager.setBorder(BorderFactory.createTitledBorder(null,
 				"Manager",
-				javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
-				javax.swing.border.TitledBorder.DEFAULT_POSITION,
-				new java.awt.Font("Tahoma", 1, 12))); // NOI18N
+				TitledBorder.DEFAULT_JUSTIFICATION,
+				TitledBorder.DEFAULT_POSITION,
+				new Font("Tahoma", 1, 12))); // NOI18N
 
-		berechnen.setIcon(new javax.swing.ImageIcon(getClass().getResource(
+		berechnen.setIcon(new ImageIcon(getClass().getResource(
 				"/bremoswing/bild/play.png"))); // NOI18N
 		berechnen.setToolTipText("berechnung ausführen");
-		berechnen.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0,
+		berechnen.setBorder(BorderFactory.createEmptyBorder(0, 0,
 				0, 0));
 		berechnen.setBorderPainted(false);
 		berechnen.setContentAreaFilled(false);
 		berechnen
-				.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+				.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 		berechnen.setVerifyInputWhenFocusTarget(false);
-		berechnen.addMouseListener(new java.awt.event.MouseAdapter() {
-			public void mousePressed(java.awt.event.MouseEvent evt) {
+		berechnen.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent evt) {
 				wahlFile.setVisible(false);
 				berechnen.setVisible(false);
 				stop.setVisible(true);
@@ -108,33 +172,33 @@ public class SwingBremo extends javax.swing.JFrame {
 			}
 		});
 		berechnen.setBounds(10, 18, 33, 33);
-		manager.add(berechnen, javax.swing.JLayeredPane.DEFAULT_LAYER);
+		manager.add(berechnen, JLayeredPane.DEFAULT_LAYER);
 
-		wahlFile.setIcon(new javax.swing.ImageIcon(getClass().getResource(
+		wahlFile.setIcon(new ImageIcon(getClass().getResource(
 				"/bremoswing/bild/folder_smart.png"))); // NOI18N
 		wahlFile.setToolTipText("InputFile auswählen");
-		wahlFile.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0,
+		wahlFile.setBorder(BorderFactory.createEmptyBorder(0, 0, 0,
 				0));
 		wahlFile.setBorderPainted(false);
 		wahlFile.setContentAreaFilled(false);
 		wahlFile.setOpaque(false);
-		wahlFile.addMouseListener(new java.awt.event.MouseAdapter() {
-			public void mousePressed(java.awt.event.MouseEvent evt) {
+		wahlFile.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent evt) {
 				wahlPush(evt);
 			}
 		});
 		wahlFile.setBounds(70, 18, 33, 33);
-		manager.add(wahlFile, javax.swing.JLayeredPane.DEFAULT_LAYER);
+		manager.add(wahlFile, JLayeredPane.DEFAULT_LAYER);
 
-		stop.setIcon(new javax.swing.ImageIcon(getClass().getResource(
+		stop.setIcon(new ImageIcon(getClass().getResource(
 				"/bremoswing/bild/stop 2.png"))); // NOI18N
 		stop.setToolTipText("Stop Bremo");
-		stop.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		stop.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 		stop.setContentAreaFilled(false);
 		stop.setMaximumSize(null);
 		stop.setMinimumSize(null);
-		stop.addMouseListener(new java.awt.event.MouseAdapter() {
-			public void mousePressed(java.awt.event.MouseEvent evt) {
+		stop.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent evt) {
 				wahlFile.setVisible(true);
 				berechnen.setVisible(true);
 				stop.setVisible(false);
@@ -143,30 +207,30 @@ public class SwingBremo extends javax.swing.JFrame {
 		});
 		stop.setVisible(false);
 		stop.setBounds(130, 18, 33, 33);
-		manager.add(stop, javax.swing.JLayeredPane.DEFAULT_LAYER);
+		manager.add(stop, JLayeredPane.DEFAULT_LAYER);
 
 		textFile.setEditable(false);
-		textFile.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0,
+		textFile.setBorder(BorderFactory.createEmptyBorder(0, 0, 0,
 				0));
 		textFile.setEnabled(false);
-		textFile.setMaximumSize(new java.awt.Dimension(0, 0));
-		textFile.setMinimumSize(new java.awt.Dimension(0, 0));
-		textFile.setPreferredSize(new java.awt.Dimension(0, 0));
+		textFile.setMaximumSize(new Dimension(0, 0));
+		textFile.setMinimumSize(new Dimension(0, 0));
+		textFile.setPreferredSize(new Dimension(0, 0));
 		textFile.setBounds(180, 20, 0, 0);
-		manager.add(textFile, javax.swing.JLayeredPane.DEFAULT_LAYER);
+		manager.add(textFile, JLayeredPane.DEFAULT_LAYER);
 
-		konsole2.setBorder(javax.swing.BorderFactory.createTitledBorder(null,
+		konsole2.setBorder(BorderFactory.createTitledBorder(null,
 				"Konsole",
-				javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
-				javax.swing.border.TitledBorder.DEFAULT_POSITION,
-				new java.awt.Font("Tahoma", 1, 12))); // NOI18N
+				TitledBorder.DEFAULT_JUSTIFICATION,
+				TitledBorder.DEFAULT_POSITION,
+				new Font("Tahoma", 1, 12))); // NOI18N
 
 		grosArea.setColumns(20);
 		grosArea.setEditable(false);
-		grosArea.setFont(new java.awt.Font("Tahoma", 3, 16)); // NOI18N
+		grosArea.setFont(new Font("Tahoma", 3, 16)); // NOI18N
 		grosArea.setRows(5);
 		grosArea.setText("Programm läuft... \nWählen Sie die Input Datei Und Dann Einfach die Berechnung Ausführen .");
-		grosArea.setMinimumSize(new java.awt.Dimension(76, 22));
+		grosArea.setMinimumSize(new Dimension(76, 22));
 		//grosArea.setPreferredSize(new  Dimension(670, 430));
 		
 		konsole2.setLayout(new GridBagLayout());
@@ -183,8 +247,8 @@ public class SwingBremo extends javax.swing.JFrame {
 
 		kleinArea.setColumns(20);
 		kleinArea.setEditable(false);
-		kleinArea.setFont(new java.awt.Font("Tahoma", 3, 13)); // NOI18N
-		kleinArea.setForeground(new java.awt.Color(255, 0, 0));
+		kleinArea.setFont(new Font("Tahoma", 3, 13)); // NOI18N
+		kleinArea.setForeground(new Color(255, 0, 0));
 		kleinArea.setRows(5);
 		//kleinArea.setPreferredSize(new Dimension(660, 140));
 		jScrollPane2.setViewportView(kleinArea);
@@ -193,56 +257,56 @@ public class SwingBremo extends javax.swing.JFrame {
 		jScrollPane2.setBounds(13, 450, 660, 180);
 		konsole2.add(jScrollPane2, c);
 
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+		GroupLayout layout = new GroupLayout(
 				getContentPane());
 		getContentPane().setLayout(layout);
 		layout.setHorizontalGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.createParallelGroup(GroupLayout.Alignment.LEADING)
 				.addGroup(
-						javax.swing.GroupLayout.Alignment.TRAILING,
+						GroupLayout.Alignment.TRAILING,
 						layout.createSequentialGroup()
 								.addContainerGap()
 								.addGroup(
 										layout.createParallelGroup(
-												javax.swing.GroupLayout.Alignment.TRAILING)
+												GroupLayout.Alignment.TRAILING)
 												.addComponent(
 														manager,
-														javax.swing.GroupLayout.Alignment.LEADING,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
+														GroupLayout.Alignment.LEADING,
+														GroupLayout.DEFAULT_SIZE,
 														685, Short.MAX_VALUE)
 												.addComponent(
 														konsole2,
-														javax.swing.GroupLayout.Alignment.LEADING,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
+														GroupLayout.Alignment.LEADING,
+														GroupLayout.DEFAULT_SIZE,
 														685, Short.MAX_VALUE))
 								.addContainerGap()));
 		layout.setVerticalGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.createParallelGroup(GroupLayout.Alignment.LEADING)
 				.addGroup(
 						layout.createSequentialGroup()
 								.addContainerGap()
 								.addComponent(manager,
-										javax.swing.GroupLayout.PREFERRED_SIZE,
+										GroupLayout.PREFERRED_SIZE,
 										60,
-										javax.swing.GroupLayout.PREFERRED_SIZE)
+										GroupLayout.PREFERRED_SIZE)
 								.addPreferredGap(
-										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+										LayoutStyle.ComponentPlacement.RELATED)
 								.addComponent(konsole2,
-										javax.swing.GroupLayout.DEFAULT_SIZE,
+										GroupLayout.DEFAULT_SIZE,
 										644, Short.MAX_VALUE).addContainerGap()));
 		pack();
 	}
 	
 
 	//Button Setting To Run Bremo Thread / to Run the calcul
-	private void BerechnungPush(java.awt.event.MouseEvent evt) {
+	private void BerechnungPush(MouseEvent evt) {
 		// TODO add your handling code here:
 		grosArea.setText("");
 		kleinArea.setText("");
 		if (textFile.getText().equals("")) {
-			javax.swing.JOptionPane.showMessageDialog(this,
+			JOptionPane.showMessageDialog(this,
 					"Überprüfen Sie Bitte die InputDatei !!!", "Achtung",
-					javax.swing.JOptionPane.WARNING_MESSAGE);
+					JOptionPane.WARNING_MESSAGE);
 			wahlFile.setVisible(true);
 			berechnen.setVisible(true);
 			stop.setVisible(false);
@@ -250,18 +314,20 @@ public class SwingBremo extends javax.swing.JFrame {
 			stop.setVisible(true);
 			control = true;
 			t1 = System.currentTimeMillis();
-			bremo = new Bremo(file);
-			bremo.start();
+			//bremo = new Bremo(file);
+			//bremo.start();
+			bremoThread = new Bremo [files.length];
+			for (int i = 0 ; i < files.length ; i++) {
+				bremoThread[i] = new Bremo(files[i]);
+				bremoThread[i].start();
+				}
 			t2 = System.currentTimeMillis();
 			// play();
 
 		}
 	}
 
-	public void play() {
-		do {
-
-		} while (!bremo.isInterrupted());
+	public static  void ActiveIcon() {
 		wahlFile.setVisible(true);
 		berechnen.setVisible(true);
 		stop.setVisible(false);
@@ -269,34 +335,37 @@ public class SwingBremo extends javax.swing.JFrame {
     
 	//Button Setting To Stop Running of Bremo Thread
 	@SuppressWarnings("static-access")
-	private void stopPush(java.awt.event.MouseEvent evt) {
+	private void stopPush(MouseEvent evt) {
 		// TODO add your handling code here:
 		if (!control) {
-			javax.swing.JOptionPane.showMessageDialog(this,
+			JOptionPane.showMessageDialog(this,
 					"Starten Sie Zu erst die Berechnung.", "Achtung",
-					javax.swing.JOptionPane.WARNING_MESSAGE);
+					JOptionPane.WARNING_MESSAGE);
 		} else {
-			if (bremo.isAlive()) {
+			/*if (bremo.isAlive()) {
 				bremo.interrupt();
 				control = false;
 			    kleinArea.append(new Timer(t2-t1).toString());
+			}*/
+			for (int i = 0 ; i < bremoThread.length; i++) {
+				if (bremoThread[i].isAlive()) {
+					bremoThread[i].interrupt();
+				}
 			}
-
+			control = false;
 		}
 	}
     
 	//Button Setting To Choose a File
-	private void wahlPush(java.awt.event.MouseEvent evt) {
+	private void wahlPush(MouseEvent evt) {
 		// TODO add your handling code here:
 		grosArea.setText("");
 		kleinArea.setText("");
 		berechnen.setEnabled(false);
-		javax.swing.JFileChooser fileChooser = new javax.swing.JFileChooser(
+		JFileChooser fileChooser = new JFileChooser(
 				path);
-		fileChooser.setFileSelectionMode(javax.swing.JFileChooser.FILES_ONLY);
-		// fileChooser.showOpenDialog(null);
-		// File[] files = chooser.getSelectedFiles();
-
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setMultiSelectionEnabled(true);
 		ExtensionFileFilter txtFilter = new ExtensionFileFilter(null,
 				new String[] { "txt" });
 
@@ -304,13 +373,14 @@ public class SwingBremo extends javax.swing.JFrame {
 		try {
 			int status = fileChooser.showOpenDialog(null);
 
-			if (status == javax.swing.JFileChooser.APPROVE_OPTION) {
+			if (status == JFileChooser.APPROVE_OPTION) {
 				if (fileChooser.getSelectedFile() != null)
 					;
-				file = fileChooser.getSelectedFile();
-				textFile.setText(file.getPath());
-				path = file.getParent();
-			} else if (status == javax.swing.JFileChooser.CANCEL_OPTION) {
+				//file = fileChooser.getSelectedFile();
+				files = fileChooser.getSelectedFiles();
+				textFile.setText(files[0].getPath());
+				path = files[0].getParent();
+			} else if (status == JFileChooser.CANCEL_OPTION) {
 				fileChooser.cancelSelection();
 			}
 		} catch (Exception e) {
@@ -374,10 +444,10 @@ public class SwingBremo extends javax.swing.JFrame {
 		 * /tutorial/uiswing/lookandfeel/plaf.html
 		 */
 		try {
-			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+			for (UIManager.LookAndFeelInfo info : UIManager
 					.getInstalledLookAndFeels()) {
 				if ("Nimbus".equals(info.getName())) {
-					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					UIManager.setLookAndFeel(info.getClassName());
 					break;
 				}
 			}
@@ -390,69 +460,17 @@ public class SwingBremo extends javax.swing.JFrame {
 		} catch (IllegalAccessException ex) {
 			java.util.logging.Logger.getLogger(SwingBremo.class.getName()).log(
 					java.util.logging.Level.SEVERE, null, ex);
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		} catch (UnsupportedLookAndFeelException ex) {
 			java.util.logging.Logger.getLogger(SwingBremo.class.getName()).log(
 					java.util.logging.Level.SEVERE, null, ex);
 		}
 
 		/* Create and display the form */
-		java.awt.EventQueue.invokeLater(new Runnable() {
+		EventQueue.invokeLater(new Runnable() {
 
 			public void run() {
 				new SwingBremo().setVisible(true);
 			}
 		});
 	}
-
-	// Variables declaration
-	private boolean control = false;
-	private java.io.File file;
-	private String path = ".";
-	private Bremo bremo;
-	private javax.swing.JButton berechnen;
-	private javax.swing.JTextArea grosArea;
-	private javax.swing.JScrollPane jScrollPane1;
-	private javax.swing.JScrollPane jScrollPane2;
-	private javax.swing.JTextArea kleinArea;
-	private JPanel konsole2;
-	private javax.swing.JLayeredPane manager;
-	private javax.swing.JButton stop;
-	private javax.swing.JTextField textFile;
-	private javax.swing.JButton wahlFile;
-	private double t1;
-	private double t2;
-	// End of variables declaration
-
-	PrintStream outStream = new PrintStream(System.out) {
-
-		@Override
-		public void println(String s) {
-
-			grosArea.append(s + "\n");
-			grosArea.setCaretPosition(grosArea.getDocument().getLength());
-		}
-
-		@Override
-		public void print(String s) {
-
-			grosArea.append(s);
-			grosArea.setCaretPosition(grosArea.getDocument().getLength());
-
-		}
-	};
-
-	PrintStream errStream = new PrintStream(System.err) {
-
-		@Override
-		public void println(String s) {
-			kleinArea.append(s + "\n");
-			kleinArea.setCaretPosition(kleinArea.getDocument().getLength());
-		}
-
-		@Override
-		public void print(String s) {
-			kleinArea.append(s);
-			kleinArea.setCaretPosition(kleinArea.getDocument().getLength());
-		}
-	};
 }

c8d9dffaf9cb7fc1148ee336bc2319cdcba35d84
ngueneko@gmail.com <ngueneko@gmail.com@95685d4f-b9f3-dcd0-96bb-d920851bcf30>
2013-12-18 17:35:40 +0000

    Final 1. good State.

---
+++ branches/Bremo 2_EN/src/numal/AE_jacobnmf_methods.java
@@ -0,0 +1,7 @@
+package numal;
+
+public interface AE_jacobnmf_methods {
+  
+  double di(int i, int m);
+  void funct(int n, int m, double x[], double f[]);
+}
---
+++ branches/Bremo 2_EN/src/numal/AP_quanewbnd_method.java
@@ -0,0 +1,6 @@
+package numal;
+
+public interface AP_quanewbnd_method {
+  
+  boolean funct(int n, int l, int u, double x[], double f[]);
+}

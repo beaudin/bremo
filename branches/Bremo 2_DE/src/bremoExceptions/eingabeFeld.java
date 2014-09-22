6b2a53d329425513c60a0c9dd3dafa2e3e798aaa
maajuus@gmail.com <maajuus@gmail.com@95685d4f-b9f3-dcd0-96bb-d920851bcf30>
2014-09-22 15:22:22 +0000

    Exceptionhandling mit Eingabemöglichkeit

--- branches/Bremo 2_DE/src/bremoExceptions/eingabeFeld.java
+++ branches/Bremo 2_DE/src/bremoExceptions/eingabeFeld.java
@@ -1,8 +1,16 @@
 package bremoExceptions;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
 import javax.swing.*;
+
 import bremo.parameter.CasePara;
 
 
@@ -13,11 +21,33 @@ public class eingabeFeld extends JFrame {
 	
 	public eingabeFeld(final CasePara cp, final String[] parameter, String vorgabe){
 		setTitle("Fehlender Parameter im Inputfile");
-		setSize(400, 200);
-        JPanel panel = new JPanel();
+		setSize(350, 100);
+		addWindowListener(new WindowAdapter() {
+		      public void windowClosing(WindowEvent e) {
+		        cp.notify();
+		      }
+		});
+        JPanel panel = new JPanel(){
+        	public void paintComponent(Graphics g) {
+	  	      
+	   	   	 URL url = getClass().getResource("/bremoswing/bild/Abstract_Frozen_Blue.jpg");
+	   			 ImageIcon icon = new ImageIcon(url);
+	   			 Image img = icon.getImage();
+	   			 BufferedImage buffImage = 
+	   			    	      new BufferedImage(
+	   			    	          img.getWidth(null), 
+	   			    	          img.getHeight(null), 
+	   			    	          BufferedImage.TYPE_INT_ARGB);
+	   			 Graphics gr = buffImage.getGraphics();
+	   			 gr.drawImage(img, 0, 0, null);
+	   			 img = buffImage.getSubimage(200, 250, getWidth(), getHeight());
+	   		     g.drawImage(img, 0, 0, null);
+	   	    }
+        };
         // Leeres JLabel-Objekt wird erzeugt
         JLabel label = new JLabel();
-        label.setText("Der Parameter \""+parameter[0]+"\" fehlt im Inputfile.\n Bitte in der Einheit \""+parameter[1]+"\" angeben:");
+        label.setText("<html><body>Der Parameter \""+parameter[0]+"\" fehlt im Inputfile.<br> Bitte in der Einheit \""+
+        		parameter[1]+"\" angeben:</body></html>");
         //Textfeld erzeugen:
         tfWert = new JTextField(vorgabe, 15);
         //OK-Button hinzufügen
@@ -27,7 +57,6 @@ public class eingabeFeld extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 				if(!tfWert.getText().replace(" ", "").equals("")){
 					cp.set_ParaInputfile(parameter[0], parameter[1], tfWert.getText());
 				}
